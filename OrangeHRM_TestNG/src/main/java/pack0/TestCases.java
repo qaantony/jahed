78a9cad430f0311f24e0ff7681379a39b234fae0
Jahed <Jahed@Jahed-HP>
2018-06-05 03:27:55 -0400

    First commit

---
+++ OrangeHRM_TestNG/src/main/java/pack0/TestCases.java
@@ -0,0 +1,29 @@
+package pack0;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+	@Test
+	public  void TC1() {
+		System.out.println("qaplanet1, lab1111");
+	}
+
+	@Test
+	public  void TC2() {
+		System.out.println("qaplanet2, lab2");
+
+	}
+
+	@Test
+	public  void TC3() {
+		System.out.println("qaplanet3, lab3");
+
+	}
+
+	@Test
+	public  void TC4() {
+		System.out.println("qaplanet4, lab4");
+
+	}
+
+}
---
+++ OrangeHRM_TestNG/src/main/java/pack0/TestCases2.java
@@ -0,0 +1,29 @@
+package pack0;
+
+import org.testng.annotations.Test;
+
+public class TestCases2 {
+@Test
+	public static void TC5() {
+		System.out.println("qaplanet5, lab5");
+	}
+
+@Test
+	public static void TC6() {
+		System.out.println("qaplanet6, lab6");
+
+	}
+
+@Test
+	public static void TC7() {
+		System.out.println("qaplanet7, lab7");
+
+	}
+
+@Test
+	public static void TC8() {
+		System.out.println("qaplanet8, lab8");
+
+	}
+
+}
