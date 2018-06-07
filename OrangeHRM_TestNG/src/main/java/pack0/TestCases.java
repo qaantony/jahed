9801d16f983c1610657eab950344beab3d376df1
qaantony <33860596+qaantony@users.noreply.github.com>
2018-06-07 16:18:32 +0530

    Update TestCases.java

--- OrangeHRM_TestNG/src/main/java/pack0/TestCases.java
+++ OrangeHRM_TestNG/src/main/java/pack0/TestCases.java
@@ -1,6 +1,9 @@
 package pack0;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestCases {
 	@Test
