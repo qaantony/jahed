4267a701f050e3176991210967394ccc129e0032
qaantony <33860596+qaantony@users.noreply.github.com>
2018-06-07 16:07:01 +0530

    Update TestCases.java

--- OrangeHRM_TestNG/src/main/java/pack0/TestCases.java
+++ OrangeHRM_TestNG/src/main/java/pack0/TestCases.java
@@ -12,6 +12,24 @@ public class TestCases {
 	@Test
 	public  void TC2() {
 		System.out.println("qaplanet2, lab2222");
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\lib\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		// accessing the qahrm project home page
+		driver.get("http://apps.qaplanet.in/qahrm/login.php");
+
+		// statements to perform login action
+		driver.findElement(By.name("txtUserName")).sendKeys("qaplanet1");
+		driver.findElement(By.name("txtPassword")).sendKeys("lab1");
+		driver.findElement(By.name("Submit")).click();
+		Thread.sleep(5000);
+
+		// statements to perform logout action
+		driver.findElement(By.linkText("Logout")).click();
+
+		// statements to quit the browser
+		driver.quit();
 
 	}
 
